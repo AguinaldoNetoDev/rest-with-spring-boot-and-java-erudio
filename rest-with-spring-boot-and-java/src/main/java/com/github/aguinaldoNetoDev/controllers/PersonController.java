48ee27f86a7d193ccb0fe50299e42b0a733a4bb7
Aguinaldo Neto <aguinaldo.netodev@gmail.com>
2026-01-03 14:11:27 -0300

    05 - The DTO design pattern

--- rest-with-spring-boot-and-java/src/main/java/com/github/aguinaldoNetoDev/controllers/PersonController.java
+++ rest-with-spring-boot-and-java/src/main/java/com/github/aguinaldoNetoDev/controllers/PersonController.java
@@ -1,6 +1,6 @@
 package com.github.aguinaldoNetoDev.controllers;
 
-import com.github.aguinaldoNetoDev.model.Person;
+import com.github.aguinaldoNetoDev.data.dto.PersonDTO;
 import com.github.aguinaldoNetoDev.services.PersonServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -19,24 +19,24 @@ public class PersonController {
     @PostMapping(
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public Person create(@RequestBody Person person) {
+    public PersonDTO create(@RequestBody PersonDTO person) {
         return service.create(person);
     }
 
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Person findById(@PathVariable("id") Long id) {
+    public PersonDTO findById(@PathVariable("id") Long id) {
         return service.findById(id);
     }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Person> findAll() {
+    public List<PersonDTO> findAll() {
         return service.findAll();
     }
 
     @PutMapping(
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public Person update(@RequestBody Person person) {
+    public PersonDTO update(@RequestBody PersonDTO person) {
         return service.update(person);
     }
 
--- rest-with-spring-boot-and-java/src/main/java/com/github/aguinaldoNetoDev/services/PersonServices.java
+++ rest-with-spring-boot-and-java/src/main/java/com/github/aguinaldoNetoDev/services/PersonServices.java
@@ -1,6 +1,8 @@
 package com.github.aguinaldoNetoDev.services;
 
+import com.github.aguinaldoNetoDev.data.dto.PersonDTO;
 import com.github.aguinaldoNetoDev.exception.ResourceNotFoundException;
+import com.github.aguinaldoNetoDev.mapper.ObjectMapper;
 import com.github.aguinaldoNetoDev.model.Person;
 import com.github.aguinaldoNetoDev.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,43 +12,55 @@ import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
+import static com.github.aguinaldoNetoDev.mapper.ObjectMapper.parseListObjects;
+import static com.github.aguinaldoNetoDev.mapper.ObjectMapper.parseObject;
+
 @Service
 public class PersonServices {
 
-    private final AtomicLong counter = new AtomicLong();
     private final Logger logger = Logger.getLogger(PersonServices.class.getName());
 
     @Autowired
     private PersonRepository repository;
 
-    public Person create(Person person) {
+    public PersonDTO create(PersonDTO dto) {
         logger.info("Save person");
-        return repository.save(person);
+
+        // parametro 1: passa o DTO para converter em uma entidade, parametro 2: passa o tipo da entidade
+        var entity = parseObject(dto, Person.class);
+
+        return parseObject(repository.save(entity), PersonDTO.class);
     }
 
-    public Person findById(Long id) {
+    public PersonDTO findById(Long id) {
         logger.info("Finding one person");
-        return repository.findById(id)
+
+        var entity = repository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("No records found for this id."));
+
+        return parseObject(entity, PersonDTO.class);
     }
 
-    public List<Person> findAll() {
+    public List<PersonDTO> findAll() {
         logger.info("Finding all people");
-        return repository.findAll();
+
+        // parametro 1: busca os dados na entidade, parametro 2: converte de entidade para DTO
+        return parseListObjects(repository.findAll(), PersonDTO.class);
     }
 
-    public Person update(Person person) {
+    public PersonDTO update(PersonDTO dto) {
         logger.info("Finding one person");
-        Person entity = repository.findById(person.getId())
+
+        Person entity = repository.findById(dto.getId())
                 .orElseThrow(() -> new ResourceNotFoundException("No records found for this id."));
 
         logger.info("Updating founded person");
-        entity.setFirstName(person.getFirstName());
-        entity.setLastName(person.getLastName());
-        entity.setAddress(person.getAddress());
-        entity.setGender(person.getGender());
+        entity.setFirstName(dto.getFirstName());
+        entity.setLastName(dto.getLastName());
+        entity.setAddress(dto.getAddress());
+        entity.setGender(dto.getGender());
 
-        return repository.save(entity);
+        return parseObject(repository.save(entity), PersonDTO.class);
     }
 
     public void delete(Long id) {
@@ -55,6 +69,7 @@ public class PersonServices {
                 .orElseThrow(() -> new ResourceNotFoundException("No records found for this id."));
 
         logger.info("Deleting one person");
+
         repository.delete(entity);
     }
 }
